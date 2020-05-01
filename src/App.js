4347c34be884c1ce6493ba3993abac7a8e39e474
Lianghan Fei <>
2020-05-01 22:40:28 +1000

    weather

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+import Header from './Header';
+import Nav from './Nav';
+import WeatherBoard from './WeatherBoard';
+import Footer from './Footer';
+
+function App() {
+  return (
+    <body>
+      <div className="weather-channel__container">
+        <Header />
+        <Nav />
+        <WeatherBoard />
+        <Footer />
+      </div>
+    </body>
+  );
+}
+
+export default App;
---
+++ src/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+// function Square(props) {
+//   return <button className="square">{props.content}</button>;
+// }
+
+class Footer extends React.Component {
+
+  render() {
+    return (
+        <footer className="weather-channel__footer">
+            <p>Powered by React</p>
+        </footer>
+    );
+  }
+}
+
+export default Footer;
